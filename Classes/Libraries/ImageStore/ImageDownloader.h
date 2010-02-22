63849eff790182bd4f74f453bec228530d5881be
Satoshi Nakagawa <psychs@limechat.net>
2010-02-22 16:30:17 +0900

    tweak

--- Classes/Libraries/ImageStore/ImageDownloader.h
+++ Classes/Libraries/ImageStore/ImageDownloader.h
@@ -1,7 +1,7 @@
 // Created by Satoshi Nakagawa.
 // You can redistribute it and/or modify it under the new BSD license.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import "HttpClient.h"
 
 @interface ImageDownloader : NSObject

783b85af3db2dbedbcf2b3e030da88328d65368f
Satoshi Nakagawa <psychs@limechat.net>
2010-05-27 08:40:23 +0900

    improved performance

--- Classes/Libraries/ImageStore/StringHelper.m
+++ Classes/Libraries/ImageStore/StringHelper.m
@@ -8,20 +8,36 @@
 
 - (NSString*)encodeAsURIComponent
 {
-	const char* p = [self UTF8String];
-	NSMutableString* result = [NSMutableString string];
+	if (!self.length) return @"";
 	
-	for (;*p ;p++) {
-		unsigned char c = *p;
-		if ('0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '-' || c == '_') {
-			[result appendFormat:@"%c", c];
+	static const char* characters = "0123456789ABCDEF";
+	
+	const char* src = [self UTF8String];
+	if (!src) return @"";
+	
+	NSUInteger len = [self lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
+	char buf[len*3];
+	char* dest = buf;
+	
+	for (NSInteger i=len-1; i>=0; --i) {
+		unsigned char c = *src++;
+		if ('a' <= c && c <= 'z'
+			|| 'A' <= c && c <= 'Z'
+			|| '0' <= c && c <= '9'
+			|| c == '_'
+			|| c == '-'
+			|| c == '.'
+			|| c == '~') {
+			*dest++ = c;
 		}
 		else {
-			[result appendFormat:@"%%%02X", c];
+			*dest++ = '%';
+			*dest++ = characters[c / 16];
+			*dest++ = characters[c % 16];
 		}
 	}
 	
-	return result;
+	return [[[NSString alloc] initWithBytes:buf length:dest - buf encoding:NSASCIIStringEncoding] autorelease];
 }
 
 @end
